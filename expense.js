066e8dc3d6182051f737f354d7d402654a1ffeac
MarkGeez <markgraelangee@gmail.com>
2026-09-09 14:18:20 +0800

    expense

--- expense.js
+++ expense.js
@@ -1,26 +1,61 @@
-
-
-function calculateTotal(expenses){
-}
-
-function calculateCategoryTotal(expenses, category) {
-}
-
-function findLargestExpense(expenses){
-    
-}
-
-
-function createExpenseSummary(expenses){
-
-}
-
 const expenses = [
   { id: 1, category: 'food', amount: 24 },
   { id: 2, category: 'transport', amount: 15 },
   { id: 3, category: 'food', amount: 18 },
   { id: 4, category: 'books', amount: 40 },
 ];
+
+function calculateTotal(expenses){
+  let total = 0;
+  
+  expenses.forEach(element => {
+    total += element.amount
+  });
+
+  return total
+}
+
+function calculateCategoryTotal(expenses, category) {
+  let total = 0;
+
+  expenses.forEach(item => {
+    if(item.category === category){
+      total += item.amount
+    }
+  })
+
+  return total
+}
+
+//should return the full expense object with the largest amount.
+function findLargestExpense(expenses){
+
+    let largest = expenses[0]
+
+    expenses.forEach(item => {
+      if(item.amount > largest.amount){
+        largest = item
+      }
+    })
+
+    return largest
+    
+}
+
+
+function createExpenseSummary(expenses){
+  return {
+    total : calculateTotal(expenses),
+    foodTotal : calculateCategoryTotal(expenses, 'food'),
+    transportTotal : calculateCategoryTotal(expenses, 'transport'),
+    largestExpense : findLargestExpense(expenses)
+  };
+
+
+}
+
+
+
 
 console.log(createExpenseSummary(expenses));
 console.log(calculateCategoryTotal(expenses, 'food'));
